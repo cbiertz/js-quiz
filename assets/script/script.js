3d54bebdbb6d9c6eb34c62179e963d2d095c6182
Chris Biertzer <c.biertzer@gmail.com>
2020-09-26 12:55:19 -0500

    quiz function works, takes time off clock, gives feedback

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,57 +1,118 @@
-let questions = [
-    {
-        question: `Does this look like a game to you?`,
-        choiceA: `Yes it does.`,
-        choiceB: `No it doesn't`,
-        choiceC: `I can't tell`,
-        choiceD: `Don't ask me!`,
-        correct: `A`
-    },{
-        question : `What are you, an idiot?`,
-        choiceA: `Yes I am.`,
-        choiceB: `No I'm not.`,
-        choiceC: `Hey, fuck you!`,
-        choiceD: `I don't know.`,
-        correct: `C`
-    },{
-        question : `How many burgers can Randy Eat?`,
-        choiceA: `1-2`,
-        choiceB: `2-3`,
-        choiceC: `3-4`,
-        choiceD: `10`,
-        correct: `D`
-    },{
-        question : `What is Lahey's current job?`,
-        choiceA: `Police officer`,
-        choiceB: `Prostitue`,
-        choiceC: `Drug dealer`,
-        choiceD: `Trailer Park Supervisor`,
-        correct: `D`
-    },{
-        question : `What grade does Ricky have?`,
-        choiceA: `Grade 9`,
-        choiceB: `Grade 10`,
-        choiceC: `Grade 11`,
-        choiceD: `Grade 12`,
-        correct: `B`
-    },{
-        question : `Who is Cory's best friend?`,
-        choiceA: `Jacob`,
-        choiceB: `Lucy`,
-        choiceC: `Trevor`,
-        choiceD: `Julien`,
-        correct: `C`
-    }
-
-]
-
-
 var timerEl = document.getElementById('countdown');
 var startBtn = document.getElementById('btnStart');
+var questionsEL = document.getElementById("questions");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var fnameEl = document.getElementById("fname");
+var feedbackEl = document.getElementById("feedback");
 
+var questions = [
+    new question ("Commonly used data types DO NOT include:", ["Strings", "Booleans", "Alerts", "Numbers"], "Alerts"),
+    new question ("The condition in an if / else statement is enclosed within ____.", ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"], "Parentheses"),
+    new question ("Arrays in JavaScript can be used to store ____.", ['Numbers and Strings', 'Other Arrays', 'Booleans', "All of the Above"], "All of the Above"),
+    new question ("String values must be enclosed within ____ when being assigned to variables.", ["Commas", "Curly Brackets", "Quotes", "Parentheses"], "Quotes"),
+    new question ("A very useful tool used during development and debugging for printing content to the debugger is:", ["JavaScript", "Terminal", "For Loops", "console.log"], "console.log"),
+];
+
+var exam = new quiz(questions);
+
+//questions
+function question (text, choices, answer) {
+    this.text=text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+question.prototype.correctAnswer = function(choice) {
+    return choice === this.answer;
+}
+
+//quiz controller
+
+function quiz (problems) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+quiz.prototype.guess = function(answer) {
+    if (this.getQuestionIndex().correctAnswer(answer)) {
+        feedbackEl.textContent = "Correct!";
+        this.score++;
+    }else {
+        feedbackEl.textContent = "Wrong!";
+        timeLeft -= 15;
+    }
+
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+  
+   
+    this.questionIndex++;
+}
+
+
+quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+    
+function startQuiz() {
+    document.getElementById("btnStart").addEventListener("click",function() {
+        document.getElementById("start-page").hidden = true;
+        document.getElementById("quiz").hidden = false;
+    });
+
+    startBtn.onclick = countdown;
+
+    getQuestions();
+}
+
+function getQuestions() {
+    if (exam.isEnded()) {
+        setTimeout(() => {
+        showScores();
+        }, 1000);
+    }
+    else {
+        setTimeout(() => {
+        // show question
+            var element = document.getElementById("question");
+            element.innerHTML = exam.getQuestionIndex().text;
+
+            // show choices
+            var choices = exam.getQuestionIndex().choices;
+            for (var i = 0; i < choices.length; i++) {
+                var element = document.getElementById("choice" + i);
+                element.innerHTML = choices[i];
+                guess("ansBtn" + i, choices[i]);
+            }
+        }, 1000);
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        exam.guess(guess);
+        getQuestions();
+    }
+}
+
+function showScores() {
+    var gameOverHtml = "<h1 >Result</h1>";
+    gameOverHtml += "<div class='result-container'><div><h3 id='score'> Your Score: " + exam.score + "</h3></div> <div> <form> <label for='fname'> <h3>Enter your name!</h3></label> <div><input type='text' id='fname' name='fname'></div><button id='btnSub'>Submit</button></form></div></div>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHtml;
+}
+
+var timeLeft= 60;
 function countdown(){
-    var timeLeft= 90;
-
     var timeInterval = setInterval(function() {
         if (timeLeft > 1) {
             timerEl.textContent = timeLeft + ' seconds remaining';
@@ -66,10 +127,8 @@ function countdown(){
     }, 1000);
 }
 
-startBtn.onclick = countdown;
+startQuiz()
 
 
-document.getElementById("btnStart").addEventListener("click",function() {
-    document.getElementById("start-page").hidden = true;
-    document.getElementById("quiz").hidden = false;
-});
+
+
